44111803c18e159f3c440e366ba65453f7b50309
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-09-08 13:45:43 +0200

    remove now unused constants

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -32,19 +32,11 @@ pub const fn deposit(items: u32, bytes: u32) -> Balance {
 /// Time and blocks.
 pub const MILLISECS_PER_BLOCK: u64 = 6000;
 pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
-pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 4 * HOURS;
-pub const EPOCH_DURATION_IN_SLOTS: u64 = {
-    const SLOT_FILL_RATE: f64 = MILLISECS_PER_BLOCK as f64 / SLOT_DURATION as f64;
-    (EPOCH_DURATION_IN_BLOCKS as f64 * SLOT_FILL_RATE) as u64
-};
 
 // These time units are defined in number of blocks.
 pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
 pub const HOURS: BlockNumber = MINUTES * 60;
 pub const DAYS: BlockNumber = HOURS * 24;
-
-// 1 in 4 blocks (on average, not counting collisions) will be primary babe blocks.
-pub const PRIMARY_PROBABILITY: (u64, u64) = (1, 4);
 
 /// Fee-related.
 /// The block saturation level. Fees will be updates based on this value.
